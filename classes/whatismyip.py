1a92d45d294d1f324c504d5463639708b0ee4527
Manuel Pedreira Antón <mpedreira@users.noreply.github.com>
2021-08-03 19:35:11 +0200

    feat: New feature for getting IP address

---
+++ classes/whatismyip.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from classes.httpRequest import *
+
+class whatIsMyIP(httpRequest):
+    def __init__(self, config):
+        endpoint = {}
+        payload = {}
+        endpoint['uri'] = 'http://whatismyip.akamai.com'
+        endpoint['certificate'] = False
+        payload['data'] = ''
+        payload['auth'] = None
+        payload['headers'] = ''
+        payload['config'] = config
+        httpRequest.__init__(self,endpoint,payload)
+
+    def getPublicIP(self):
+        self.getRequest()
+        ip = self.response.text
+        return ip
