60f0f6212e784c290c4525d6304078684d6ae52a
Manuel Pedreira Antón <mpedreira@users.noreply.github.com>
2021-08-03 20:59:41 +0200

    refactor: cleanning code for easy reading

--- downloadSpeedExample.py
+++ downloadSpeedExample.py
@@ -18,6 +18,34 @@ NUM_ERRORS = 8
 SLEEP = 10
 METRIC = 'Velocidad en CASA'
 
+def setGrafanaInstance(config):
+    endpoint = {}
+    endpoint['apikey']='174504:eyJrIjoiODVmYjc5NWI0M2Y5YzU5YmUzYjZjMGNkYzMxMTFmYjllYjE1NTg0MSIsIm4iOiJzcGVlZFRlc3QiLCJpZCI6NTI3NzM1fQ=='
+    endpoint['url']='https://graphite-blocks-prod-us-central1.grafana.net/graphite/metrics'
+    grafanaInstance=grafanaCloud(config, endpoint)
+    return grafanaInstance
+
+def setHttpInstance(config):
+    endpoint = {}
+    payload = {}
+    endpoint[
+    'uri'] = 'https://content.rolex.com/dam/homepage/hss/watches/classic-watches/day-date/day-date-40/homepage-day-date-40-m228238-0042.mp4'
+# endpoint[
+#    'uri'] = 'https://releases.ubuntu.com/20.04.2.0/ubuntu-20.04.2.0-desktop-amd64.iso'
+    endpoint['certificate'] = False
+    payload['data'] = ''
+    payload['auth'] = ''
+    payload['config'] = config
+    payload['headers'] = None
+    httpInstance = httpRequest(endpoint, payload)
+    return httpInstance
+
+def setGrafanaData(grafanaInstance, date, speed):
+    metrics = [
+        (METRIC, SLEEP, speed, date)
+    ]
+    grafanaInstance.setMetrics(metrics)
+
 # MAIN
 config = configurationChangeManagement()
 log = logger(config, 'main')
@@ -28,40 +56,10 @@ date = datetime.datetime.now()
 now = date.strftime('%d/%m/%Y %H:%M:%S')
 log.setInfo('Running at ' + now)
 ip = whatIsMyIP(config)
-endpoint = {}
-endpoint['apikey']='174504:eyJrIjoiODVmYjc5NWI0M2Y5YzU5YmUzYjZjMGNkYzMxMTFmYjllYjE1NTg0MSIsIm4iOiJzcGVlZFRlc3QiLCJpZCI6NTI3NzM1fQ=='
-endpoint['url']='https://graphite-blocks-prod-us-central1.grafana.net/graphite/metrics'
-grafanaInstance=grafanaCloud(config, endpoint)
-endpoint = {}
-payload = {}
-endpoint[
-    'uri'] = 'https://content.rolex.com/dam/homepage/hss/watches/classic-watches/day-date/day-date-40/homepage-day-date-40-m228238-0042.mp4'
-# endpoint[
-#    'uri'] = 'https://releases.ubuntu.com/20.04.2.0/ubuntu-20.04.2.0-desktop-amd64.iso'
-endpoint['certificate'] = False
-payload['data'] = ''
-payload['auth'] = ''
-payload['config'] = config
-payload['headers'] = None
-httpInstance = httpRequest(endpoint, payload)
-j = 0
-
+httpInstance = setHttpInstance(config)
+grafanaInstance = setGrafanaInstance(config)
 while True:
-    i = 0
-    while i >= 0:
-        date = datetime.datetime.now()
-        speed = httpInstance.speedTest()
-        metrics = [
-            (METRIC, SLEEP, speed, date)
-        ]
-        grafanaInstance.setMetrics(metrics)
-        if (speed < MINIMUM_SPEED):
-            i = i + 1
-            if i > NUM_ERRORS:
-                log.setInfo('Llevamos más de ' + str(NUM_ERRORS) +
-                            ' por debajo del objetivo de ' +
-                            str(MINIMUM_SPEED))
-                i = -1
-        else:
-            i = -1
-        time.sleep(SLEEP)
+    date = datetime.datetime.now()
+    speed = httpInstance.speedTest()
+    setGrafanaData(grafanaInstance, date, speed)
+    time.sleep(SLEEP)
