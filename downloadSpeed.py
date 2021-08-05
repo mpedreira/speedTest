5a813a8f830f7b9af02f9dc07ba87a6853655484
Manuel Pedreira <mpedreira@users.noreply.github.com>
2021-08-05 09:43:43 +0200

    feat: send email

--- downloadSpeed.py
+++ downloadSpeed.py
@@ -3,6 +3,7 @@
 from classes.jira import *
 from classes.configurationChangeManagement import *
 from classes.logger import *
+from classes.mail import *
 from classes.httpRequest import *
 import datetime
 import csv
@@ -12,7 +13,8 @@ import ssl
 from classes.jsd import *
 
 MINIMUM_SPEED = 100
-NUM_ERRORS = 8
+NUM_ERRORS = 3
+SLEEP = 60
 
 # MAIN
 config = configurationChangeManagement()
@@ -24,11 +26,13 @@ date = datetime.datetime.now()
 now = date.strftime('%d/%m/%Y %H:%M:%S')
 log.setInfo('Running at ' + now)
 endpoint = {}
+endpoint['server'] = 'smtp.office365.com'
+endpoint['port'] = 587
+mailInstance = mail(config, endpoint)
+endpoint = {}
 payload = {}
 endpoint[
     'uri'] = 'https://content.rolex.com/dam/homepage/hss/watches/classic-watches/day-date/day-date-40/homepage-day-date-40-m228238-0042.mp4'
-# endpoint[
-#    'uri'] = 'https://releases.ubuntu.com/20.04.2.0/ubuntu-20.04.2.0-desktop-amd64.iso'
 endpoint['certificate'] = False
 payload['data'] = ''
 payload['auth'] = ''
@@ -44,10 +48,35 @@ while True:
         if (speed < MINIMUM_SPEED):
             i = i + 1
             if i > NUM_ERRORS:
+                payloadEmail = {}
+                payloadEmail[
+                    'subject'] = 'Problemas de rendimiento de la wifi de Zara.com'
+                payloadEmail['html'] = """\
+                <html>
+                <head></head>
+                <body>
+                    <p>Hola!<br>
+                    Te envio este correo porque el rendimiento de la wifi 
+                    de Zara.com no ha sido bueno en los ultimos minutos.
+                    La ultima metrica es de
+                """ + str(speed) + """\
+                     que esta por debajo de 
+                """ + str(MINIMUM_SPEED) + """\
+                    <br>
+                    Si la incidencia continua escaladselo a JC
+                    </p>
+                </body>
+                </html>
+                """
+
+                payloadEmail['receiver'] = [
+                    'manuelpa@inditex.com', 'xabiergd@inditex.com'
+                ]
                 log.setInfo('Llevamos más de ' + str(NUM_ERRORS) +
                             ' por debajo del objetivo de ' +
                             str(MINIMUM_SPEED))
+                mailInstance.sendHTMLEmail(payloadEmail)
                 i = -1
         else:
             i = -1
-        time.sleep(10)
+        time.sleep(SLEEP)
