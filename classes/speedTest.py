298007eb3f3633f3f0cc10954807520c6a77fbda
Manuel Pedreira <mpedreira@users.noreply.github.com>
2021-09-09 09:06:00 +0200

    fix: some fixes of calculations.

--- classes/speedTest.py
+++ classes/speedTest.py
@@ -11,7 +11,7 @@ import requests
 from classes.basics import *
 from classes.logger import logger
 
-SERVER = '21378'
+SERVER = '41201'
 
 
 class speedTest:
@@ -33,6 +33,14 @@ class speedTest:
                 'This command requires speedTestCLI https://www.speedtest.net/apps/cli'
             )
             return speedtestResult
+        download = speedtestResult['download']['bandwidth']
+
+        upload = speedtestResult['upload']['bandwidth']
+        latency = speedtestResult['ping']['latency']
+        self.log.setInfo('Enviados datos por el server ' + self.server +
+                         ': Download -> ' + str(round(download, 2)) +
+                         ' Mbps Upload -> ' + str(round(upload, 2)) +
+                         ' Mbps Latency -> ' + str(round(latency, 2)) + ' ms')
         return speedtestResult
 
     def __getSpeed(self, speed):
